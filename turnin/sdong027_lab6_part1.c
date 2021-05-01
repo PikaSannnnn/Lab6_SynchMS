f968212efa98882ef380386e9878d67e19f136e9
PikaSannnnn <Dong.Stephen11@gmail.com>
2021-05-01 15:53:55 -0700

    Added Demo Link

--- turnin/sdong027_lab6_part1.c
+++ turnin/sdong027_lab6_part1.c
@@ -2,7 +2,7 @@
  *  Partner(s) Name: 
  *	Lab Section:
  *	Assignment: Lab #6  Exercise #1
- *	Exercise Description: [optional - include for your own benefit]
+ *	Exercise Description: Demo: https://www.youtube.com/watch?v=pZ-tNajHszk
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
